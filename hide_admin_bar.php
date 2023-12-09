13aea8749f191e3d1839963cd78c11becccd7fe7
MapSteps <connect@mapsteps.com>
2023-12-09 01:42:12 +0100

    formatting

--- hide_admin_bar.php
+++ hide_admin_bar.php
@@ -17,15 +17,17 @@
  * Hide admin bar preferences in the users profile.
  */
 function hide_admin_bar_prefs() {
+
 	?>
 	<style type="text/css">
 		.show-admin-bar {display: none;}
 	</style>
 	<?php
+
 }
-add_action('admin_print_scripts-profile.php', 'hide_admin_bar_prefs');
+add_action( 'admin_print_scripts-profile.php', 'hide_admin_bar_prefs' );
 
 /**
  * Disable admin bar.
  */
-add_filter('show_admin_bar', '__return_false');
+add_filter( 'show_admin_bar', '__return_false' );
